7a15b03e2d91bafc0bbd6f6e94654e147e6ee3ea
Thiyagesh <60864803+thiyageshvenkat@users.noreply.github.com>
2026-03-22 14:49:17 -0700

    Refactor competitest.nvim template

--- templates/competitest.cpp
+++ templates/competitest.cpp
@@ -7,13 +7,27 @@
   Start:   $(DATE)
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
+/* #include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <numeric>
+*/
+
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
 using namespace std;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  
-  return 0;
+    FASTIO;
+    return 0;
 }
-
